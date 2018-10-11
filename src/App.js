c9c89a3ae376b3655923b55eccb43f87f4c56ede
Paul Walsh <twentytwentyk@live.com>
2018-10-11 14:14:00 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import Navigation from './components/Navigation';
+import LandingPage from './components/Landing';
+import SignUpPage from './components/SignUp';
+import SignInPage from './components/SignIn';
+import PasswordForgetPage from './components/PasswordForget';
+import HomePage from './components/Home';
+import AccountPage from './components/Account';
+import * as routes from './constants/routes';
+
+const App = () =>
+  <Router>
+    <div>
+      <Navigation />
+      <hr/>
+
+      <Route
+        exact path={routes.LANDING}
+        component={LandingPage}
+      />
+      <Route
+        exact path={routes.SIGN_UP}
+        component={SignUpPage}
+      />
+      <Route
+        exact path={routes.SIGN_IN}
+        component={SignInPage}
+      />
+      <Route
+        exact path={routes.PASSWORD_FORGET}
+        component={PasswordForgetPage}
+      />
+      <Route
+        exact path={routes.HOME}
+        component={HomePage}
+      />
+      <Route
+        exact path={routes.ACCOUNT}
+        component={AccountPage}
+      />
+    </div>
+  </Router>
 
 export default App;
